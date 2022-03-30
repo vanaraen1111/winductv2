c05c16e60113edfbed77c64e3049ae566afbb195
Van <vodkachoke65@gmail.com>
2022-03-31 02:47:43 +0800

    update navbar and footer

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,48 @@
-$(document).ready(function(){$("#ftr").load("footer.html",()=>{$(".currentYear").text((new Date).getFullYear())});new Swiper("#hero",{pagination:".swiper-pagination",paginationClickable:!0,nextButton:".swiper-button-next",prevButton:".swiper-button-prev",slidesPerView:1,parallax:!0,speed:1e3}),new Swiper("#testimonials_slider",{pagination:".swiper-pagination",paginationClickable:!0,nextButton:".swiper-button-next",prevButton:".swiper-button-prev",parallax:!0,speed:1e3});$('[data-pages="search"]').search({searchField:"#overlay-search",closeButton:".overlay-close",suggestions:"#overlay-suggestions",brand:".brand",onSearchSubmit:function(e){console.log("Search for: "+e)},onKeyEnter:function(e){console.log("Live search for: "+e);var t=$("#overlay-search"),a=$(".search-results");clearTimeout($.data(this,"timer")),a.fadeOut("fast");var n=setTimeout(function(){a.find(".result-name").each(function(){0!=t.val().length&&($(this).html(t.val()),a.fadeIn("fast"))})},500);$(this).data("timer",n)}})});
+$(document).ready(function() {
+            $("#ftr").load("footer.html",()=>{
+              $(".currentYear").text(new Date().getFullYear());
+          });
+    'use strict';
+    var slider = new Swiper('#hero', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        parallax: true,
+        speed: 1000,
+    });
+    var testimonials_slider = new Swiper('#testimonials_slider', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        parallax: true,
+        speed: 1000
+    });
+    $('[data-pages="search"]').search({
+        searchField: '#overlay-search',
+        closeButton: '.overlay-close',
+        suggestions: '#overlay-suggestions',
+        brand: '.brand',
+        onSearchSubmit: function(searchString) {
+            console.log("Search for: " + searchString);
+        },
+        onKeyEnter: function(searchString) {
+            console.log("Live search for: " + searchString);
+            var searchField = $('#overlay-search');
+            var searchResults = $('.search-results');
+            clearTimeout($.data(this, 'timer'));
+            searchResults.fadeOut("fast");
+            var wait = setTimeout(function() {
+                searchResults.find('.result-name').each(function() {
+                    if (searchField.val().length != 0) {
+                        $(this).html(searchField.val());
+                        searchResults.fadeIn("fast");
+                    }
+                });
+            }, 500);
+            $(this).data('timer', wait);
+        }
+    });
+});
